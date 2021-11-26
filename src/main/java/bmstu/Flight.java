dac200fd747c6f3ae2051bd2048a40188e07a055
AlexMartPr <alexmartpr@gmail.com>
2021-11-27 01:00:12 +0300

    Scripted auto-commit on change (2021-11-27 01:00:12) by gitwatch.sh

--- src/main/java/bmstu/Flight.java
+++ src/main/java/bmstu/Flight.java
@@ -3,27 +3,15 @@ package bmstu;
 import java.io.Serializable;
 
 public class Flight implements Serializable {
-    private String destID;
-    private String originName;
     private double delay;
     private boolean cancelled;
 
-    public Flight(String dID, String oN,  Double del, Boolean cancl) {
-        this.destID = dID;
-        this.originName = oN;
+    public Flight(Double del, Boolean cancl) {
         this.delay = del;
         this.cancelled = cancl;
     }
 
     public Flight() {};
-
-    public String getDestinationID() {
-        return this.destID;
-    }
-
-    public String getOriginName() {
-        return this.originName;
-    }
 
     public double getDelay() {
         return this.delay;
