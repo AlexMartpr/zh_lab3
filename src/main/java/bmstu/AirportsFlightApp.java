29d25f74b626bed5790c074a7d709e7ab0dc9da4
AlexMartPr <alexmartpr@gmail.com>
2021-11-27 00:13:41 +0300

    Scripted auto-commit on change (2021-11-27 00:13:41) by gitwatch.sh

--- src/main/java/bmstu/AirportsFlightApp.java
+++ src/main/java/bmstu/AirportsFlightApp.java
@@ -1,6 +1,7 @@
 package bmstu;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
