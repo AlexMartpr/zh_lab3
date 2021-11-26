13234ba5d67cd7cf11550ddf869136efbf6d0b1f
AlexMartPr <alexmartpr@gmail.com>
2021-11-27 00:21:29 +0300

    Scripted auto-commit on change (2021-11-27 00:21:29) by gitwatch.sh

--- src/main/java/bmstu/Flight.java
+++ src/main/java/bmstu/Flight.java
@@ -3,5 +3,13 @@ package bmstu;
 import java.io.Serializable;
 
 public class Flight implements Serializable {
-    
+    private String destID;
+    private double delay;
+    private boolean cancelled;
+
+    public Flight(String dID, String del, String cancl) {
+        this.destID = dID;
+        this.delay = del;
+        this.cancelled = cancl;
+    }
 }
